3505e1859105caf5630a22ae6b98a0351bf29e9e
liubincodelife <liubinmcu@sina.com>
2019-06-15 23:57:16 +0800

    implement leetcode292

---
+++ Leetcode292_NimGame/NimGame.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool canWinNim(int n)
+    {
+        return (n % 4 != 0);
+    }
+protected:
+private:
+};
+
+int main(int argc, char** argv)
+{
+    getchar();
+    return 0;
+}
